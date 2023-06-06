df77320fe6525c046dbd989e2b3bd5c75c0b94a5
shreyas0123 <shreyasgowdaab@gmailcom>
2023-06-06 17:54:56 +0530

    using sequelizer in sql

--- models/product.js
+++ models/product.js
@@ -1,32 +1,27 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart');
+const sequelize = require('../util/database'); //creating object
 
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {     //this is product model and 'product(basically product name i gave)
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncreament: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price:{
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false
   }
+});
 
-  save() {
-    return db.execute('INSERT INTO product (title,price,imageUrl,description) VALUES(?, ?, ?, ?)',
-    [this.title, this.price, this.imageUrl, this.description]
-    ); 
-  }
-
-  static deleteById(id){
-   return db.execute('DELETE FROM product WHERE product.id = ?', [id]);
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM product');
-  }
-
-  static findById(id) {
-  return db.execute('SELECT * FROM product WHERE product.id = ?',[id]);
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-completes',
-    password:'root123'
+const sequelize = new Sequelize('node-completes','root','root123',{
+    dialect:'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
-
+module.exports = sequelize;
